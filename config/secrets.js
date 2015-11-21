488d2cab74a9916eb76543d7bdd1ec4e897f0751
Nandini Chitale <nchitale@ucla.edu>
2015-11-21 12:29:25 -0800

    created nandini's branch and changed server

--- config/secrets.js
+++ config/secrets.js
@@ -21,7 +21,7 @@
 
 module.exports = {
 
-  db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test',
+  db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/test',
 
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
 
